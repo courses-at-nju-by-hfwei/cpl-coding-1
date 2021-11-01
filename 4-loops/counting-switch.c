8028ce3d8e6d3e7c6bf0c700a0b8356fae2f3795
hengxin <hfwei@nju.edu.cn>
2021-11-01 14:01:08 +0800

    4-loops: +code

--- 4-loops/counting-switch.c
+++ 4-loops/counting-switch.c
@@ -2,7 +2,7 @@
  * Count the number of occurrences of each digit,
  * of white space characters, and of all other characters.
  *
- * Created by hengxin on 10/30/21.
+ * Created by hengxin on 10/16/21.
  */
 
 #include <stdio.h>
@@ -18,15 +18,30 @@ int main() {
    * Note: fails to run this program in "Run"
    * See: https://youtrack.jetbrains.com/issue/CPP-5704
    * Use "Terminal" instead.
-   * Or use the "input redirection" (output redirection) technique.
+   * Or use the "input redirection" technique.
    */
+  char ch;
+  while (scanf("%c", &ch) != EOF) {
+    switch (ch) {
+      case '0': case '1': case '2': case '3': case '4':
+      case '5': case '6': case '7': case '8': case '9':
+        digit_count[ch - '0']++;
+        break;
+      case ' ': case '\n': case '\t':
+        ws_count++;
+        break;
+      default:
+        other_count++;
+        break;
+    }
+  }
 
   printf("digit_count:");
   for (int i = 0; i < LEN; i++) {
-    printf("%d : %d", i, digit_count[i]);
+    printf(" %d", digit_count[i]);
   }
-  printf("\nws_count : %d\n", ws_count);
-  printf("other_count : %d\n", other_count);
+  printf("\nws_count: %d\n", ws_count);
+  printf("other_count: %d\n", other_count);
 
   return 0;
 }
--- 4-loops/game-of-life.c
+++ 4-loops/game-of-life.c
@@ -10,16 +10,87 @@
 
 #include <stdio.h>
 #include <unistd.h>
+
+#define ROUND 10
 #define SIZE 6
 int board[SIZE][SIZE] = {
     {0},
-    {0, 1, 1, 0, 0, 0},
-    {0, 1, 1, 0, 0, 0},
-    {0, 0, 0, 1, 1, 0},
-    {0, 0, 0, 1, 1, 0},
+    {0},
+    {0, 0, 1, 1, 1, 0},
+    {0, 1, 1, 1, 0, 0},
+    {0},
     {0}};
 
 int main() {
+    /**
+     * (extended) old board
+     */
+    int old_board[SIZE + 2][SIZE + 2];
+    for (int row = 0; row < SIZE + 2; row++) {
+      for (int col = 0; col < SIZE + 2; col++) {
+        if (row == 0 || row == SIZE + 1 || col == 0 || col == SIZE + 1) {
+          old_board[row][col] = 0;
+        } else {
+          old_board[row][col] = board[row - 1][col - 1];
+        }
+      }
+    }
+
+    /**
+     * print the current configuration
+     */
+    for (int row = 1; row < SIZE + 1; row++) {
+      for (int col = 1; col < SIZE + 1; col++) {
+        printf("%c ", old_board[row][col] ? '*' : ' ');
+      }
+      printf("\n");
+    }
+    /**
+     * windows: clrscr() (#include <conio.h>)
+     */
+    printf("\033c");
+
+    /*
+     * new board in the next round
+     */
+  int new_board[SIZE + 2][SIZE + 2];
+
+  for (int round = 0; round < ROUND; round++) {
+    for (int row = 1; row < SIZE + 1; row++) {
+      for (int col = 1; col < SIZE + 1; col++) {
+        int neighbours = old_board[row - 1][col - 1]
+            + old_board[row - 1][col]
+            + old_board[row - 1][col + 1]
+            + old_board[row][col - 1]
+            + old_board[row][col + 1]
+            + old_board[row + 1][col - 1]
+            + old_board[row + 1][col]
+            + old_board[row + 1][col + 1];
+
+        new_board[row][col] =
+            (old_board[row][col] && (neighbours == 2 || neighbours == 3))
+                || (!old_board[row][col] && neighbours == 3);
+      }
+    }
+
+    /**
+     * print the new configuration
+     */
+    for (int row = 1; row < SIZE + 1; row++) {
+      for (int col = 1; col < SIZE + 1; col++) {
+        printf("%c ", new_board[row][col] ? '*' : ' ');
+      }
+      printf("\n");
+    }
+    sleep(1);
+    printf("\033c");
+
+    for (int row = 1; row < SIZE + 1; row++) {
+      for (int col = 1; col < SIZE + 1; col++) {
+        old_board[row][col] = new_board[row][col];
+      }
+    }
+  }
 
   return 0;
 }
