9d83e9446069c3f936007cee908f551723d663f1
hengxin <hfwei@nju.edu.cn>
2021-12-13 13:50:43 +0800

    10-struct: +code in class

--- 10-struct/echo.c
+++ 10-struct/echo.c
@@ -8,23 +8,43 @@
 
 #include <stdio.h>
 
+/**
+ *
+ * @param argc argument count
+ * @param argv argument vector
+ *  By convention, argv[0] is the name of the program.
+ *  By the C standard, argv[argc] is NULL.
+ * @return
+ *
+ * char *argv[] : char **argv
+ */
 int main(int argc, char *argv[]) {
-  printf("%d\n", argc);
+//  printf("argc = %d\n", argc);
 
   /**
    * 1st version: treats argv as an array of character pointers
    */
+  for (int i = 1; i < argc; i++) {
+    printf("%s%s", argv[i], (i < argc - 1) ? " " : "");
+  }
   printf("\n");
 
   /**
    * 2st version: treat argv as a pointer to pointer to char
    */
+  while (--argc > 0) {
+    printf("%s%s", *++argv, (argc > 1) ? " " : "");
+  }
   printf("\n");
 
   /**
    * 3rd version: use an expression as the format argument of printf
+   * Note: argc = 0 now!
    */
-  printf("\n");
+//  while (--argc > 0) {
+//    printf((argc > 1) ? "%s " : "%s", *++argv);
+//  }
+//  printf("\n");
 
   return 0;
 }
